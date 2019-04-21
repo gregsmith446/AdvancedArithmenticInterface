3fdc132f32fe73497165ae6ac9495103e7fd5a95
Greg Smith <gregsmith446@gmail.com>
2019-04-21 05:49:13 -0400

    this challenge passes all 6 test cases

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _19Interfaces
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give int now: ");
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+        }
+    }
+
+
+    public interface AdvancedArithmetic
+    {
+        int divisorSum(int n);
+    }
+
+    public class Calculator : AdvancedArithmetic
+    {
+        public int divisorSum(int n)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
+
